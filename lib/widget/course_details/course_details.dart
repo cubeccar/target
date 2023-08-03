2a6c5f17dd47d2a570c3b4a958de309ebaf0eb70
cubeccar <135095789+cubeccar@users.noreply.github.com>
2023-08-03 12:51:47 +0530

    Code review

--- lib/widget/course_details/course_details.dart
+++ lib/widget/course_details/course_details.dart
@@ -1,3 +1,12 @@
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/material.dart';
+/*
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:provider/provider.dart';
+import 'package:window_size/window_size.dart';
+
+*/
+
 class CourseDetails extends StatelessWidget {
   const CourseDetails({Key key}) : super(key: key);
 
@@ -21,6 +30,16 @@ class CourseDetails extends StatelessWidget {
             'In this course we will go over the basics of using Flutter Web for website development. Topics will include Responsive Layout, Deploying, Font Changes, Hover Functionality, Modals and more.',
             style: TextStyle(fontSize: 21, height: 1.7),
           ),
+          TextButton(
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all(Colors.blue),
+                      overlayColor:
+                          MaterialStateProperty.all(Colors.transparent),
+                    ),
+                    onPressed: () =>
+                        launchUrl(Uri.parse('/index.html')),
+                    child: const Text('Provider Bloc'),
+            ),
         ],
       ),
     );
